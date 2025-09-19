6669b5e2f56cf601461e32e8bcd99ecd569827c5
wayTami-O <benefactor304@gmail.com>
2025-09-20 01:36:37 +0400

    fix burger

--- src/page/home/sections/header.tsx
+++ src/page/home/sections/header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 function Header() {
 
-    const [open, setOpen] = useState<boolean>(true)
+    const [open, setOpen] = useState<boolean>(false)
 
     return (
         <>
